7674e5695211e2a607c624a15f26e9d284356eec
Keiquan Blackmon <120697653+Yepitskeys@users.noreply.github.com>
2023-05-21 19:24:29 -0400

    added timer and quizbox

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,62 @@
 var CountdownTimer = document.getElementById('countdown');
 var StartButton = document.getElementById('start');
+var StartUp = document.getElementById('startup');
+var Results = document.getElementById('results');
+var Choices = document.getElementById('choices');
+var Questions = document.getElementById('questions');
 
 StartButton.addEventListener('click', function() {
-	var timeLeft = 31;
+    // hides the startup intro paragraph
+    document.getElementById('startup').hidden = true;
+    document.getElementById('start').hidden = true;
+
+    QuestionBox();
+    var timeLeft = 31;
 	
 	var timerId = setInterval(function() {
         timeLeft--;
-        CountdownTimer.textContent = timeLeft + " Seconds Left!";
-
+        CountdownTimer.textContent = "Time Left: " + timeLeft;
+    
     if (timeLeft === 0) {
         clearInterval(timerId);
         sendMessage()
+        score()
     }
 	}, 1000);
 });
+
+
 
 function sendMessage(){
     CountdownTimer.textContent = "GAME OVER!!";
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function score(){
+    Results.textContent = "Your Score is " + " !!!";
+    // MAKE SURE TO CREATE FUNCTION TO CALCULATE SCORE
+}
+// Function to pull questions
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+  
+    var Title = document.getElementById('question-title');
+    Title.textContent = currentQuestion.title;
+  
+    Choices.innerHTML = '';
+  
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+      // create new button for each choice
+      var choice = currentQuestion.choices[i];
+      var choiceNode = document.createElement('button');
+      choiceNode.setAttribute('class', 'choices');
+      choiceNode.setAttribute('value', choice);
+  
+      choiceNode.textContent = i + 1 + '. ' + choice;
+  
+      // display on the page
+      Answers.appendChild();
+    }
+  }
 
 var questions = [
 {
